01d881d8614497257522fb7df86373431527c1f5
decabral <deborahcabralsilva@gmail.com>
2026-04-08 18:36:06 -0300

    Inicializando

---
+++ Module 01 - Code Cultivation/ex5/ft_plant_types.py
@@ -0,0 +1,7 @@
+class Plant:
+    def __init__(self, name: str, height: float, 
+                 days_age: int) -> None:
+        self.name = name
+        self.height = height
+        self.days_age = days_age
+        
